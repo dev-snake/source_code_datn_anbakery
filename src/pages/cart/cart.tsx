cbec5e936adb9bcff28403c4191173ea84c3fcdd
Đặng Văn Hậu <121596451+dev-snake@users.noreply.github.com>
2024-12-12 19:05:22 +0700

    initial commit

---
+++ src/pages/cart/cart.tsx
@@ -0,0 +1,28 @@
+import iconConfig from "@/config/icons/icon-config";
+import { Button } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
+import BranchCart from "./branch-cart";
+import clientRoutes from "@/config/routes/client-routes.config";
+
+const Cart = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="mx-auto w-[1280px] overflow-auto pt-8 max-lg:px-2">
+      <div className="flex items-center justify-between">
+        <h1 className="text-default-300">GIỎ HÀNG CỦA BẠN</h1>
+        <Button
+          startContent={iconConfig.back.base}
+          color="secondary"
+          size="lg"
+          onClick={() => navigate(clientRoutes.cakes.root)}
+        >
+          Quay lại
+        </Button>
+      </div>
+      <BranchCart />
+    </div>
+  );
+};
+
+export default Cart;
